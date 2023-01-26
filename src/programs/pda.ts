5a567705cd782df8c2057f62053479f1864dbf4a
Giannis Chatziveroglou <gchatz@mit.edu>
2023-01-25 18:04:21 -0800

    change on pda derivation

--- src/programs/pda.ts
+++ src/programs/pda.ts
@@ -1,3 +1,4 @@
+import { tryPublicKey } from "@cardinal/common";
 import { utils } from "@project-serum/anchor";
 import { PublicKey } from "@solana/web3.js";
 
@@ -7,9 +8,15 @@ import { CONFIG_ENTRY_SEED, CONFIGS_ADDRESS } from "./constants";
  * Finds the config entry id.
  * @returns
  */
-export const findConfigEntryId = (key: string): PublicKey => {
+export const findConfigEntryId = (key: string, prefix?: string): PublicKey => {
   return PublicKey.findProgramAddressSync(
-    [utils.bytes.utf8.encode(CONFIG_ENTRY_SEED), utils.bytes.utf8.encode(key)],
+    [
+      utils.bytes.utf8.encode(CONFIG_ENTRY_SEED),
+      utils.bytes.utf8.encode(prefix ? `${prefix}:` : ""),
+      tryPublicKey(key)
+        ? new PublicKey(key).toBuffer()
+        : utils.bytes.utf8.encode(key),
+    ],
     CONFIGS_ADDRESS
   )[0];
 };
