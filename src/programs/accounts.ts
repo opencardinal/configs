f414641815633970c68fdee5a3d6aa871b70d1c8
Giannis Chatziveroglou <gchatz@mit.edu>
2023-01-30 10:18:59 -0800

    getConfigEntryById

--- src/programs/accounts.ts
+++ src/programs/accounts.ts
@@ -1,6 +1,6 @@
 import type { AccountData } from "@cardinal/common";
 import { fetchIdlAccount } from "@cardinal/common";
-import type { Connection } from "@solana/web3.js";
+import type { Connection, PublicKey } from "@solana/web3.js";
 
 import type { ConfigEntryData, CONFIGS_PROGRAM } from "./constants";
 import { CONFIGS_IDL } from "./constants";
@@ -12,6 +12,18 @@ export const getConfigEntry = async (
   key: Buffer
 ): Promise<AccountData<ConfigEntryData>> => {
   const configEntryId = findConfigEntryId(prefix, key);
+  return fetchIdlAccount<"configEntry", CONFIGS_PROGRAM>(
+    connection,
+    configEntryId,
+    "configEntry",
+    CONFIGS_IDL
+  );
+};
+
+export const getConfigEntryById = async (
+  connection: Connection,
+  configEntryId: PublicKey
+): Promise<AccountData<ConfigEntryData>> => {
   return fetchIdlAccount<"configEntry", CONFIGS_PROGRAM>(
     connection,
     configEntryId,
